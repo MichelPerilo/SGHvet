8a202acbdb0bf14bba772e7209da3c795b1cc152
MichelPerilo <michel_perilo@hotmail.com>
2017-11-13 16:27:08 -0300

    UI Login - Refatoração

--- src/br/sghvet/application/Main.java
+++ src/br/sghvet/application/Main.java
@@ -1,16 +1,37 @@
 package br.sghvet.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
-		
-	}
+	
+Stage window;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("SGHvet");
+        Parent root = FXMLLoader.load(getClass().getResource("../view/fxml_ui_login.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../../assets/imgs/sghvet_v2_h_rgb.png")));
+        //stage.setResizable(false);
+        stage.show();
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+	
+
 }
--- src/br/sghvet/controller/UiLoginController.java
+++ src/br/sghvet/controller/UiLoginController.java
@@ -3,16 +3,35 @@ package br.sghvet.controller;
 import javafx.fxml.FXML;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
-public class UiLoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ */
+public class UiLoginController implements Initializable{
 	
 	@FXML
 	private AnchorPane login_component;
 	
-	
-	
-	
-	DropShadow dropshadow = new DropShadow();
+
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		DropShadow dropShadow = new DropShadow();
+		 dropShadow.setRadius(10.0);
+		 dropShadow.setOffsetX(1.0);
+		 dropShadow.setOffsetY(1.0);
+		 dropShadow.setColor(Color.color (0.4, 0.4, 0.4, 0.4));
+		 
+		 login_component.setEffect(dropShadow);
+		
+	}
 	 
 	 
 	 
