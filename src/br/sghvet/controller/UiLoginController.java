9f6e0e2d0968a30ad5a1c2b8c40ed722497532d8
MichelPerilo <michel_perilo@hotmail.com>
2017-11-13 12:11:19 -0300

    UI Login

--- src/br/sghvet/controller/UiLoginController.java
+++ src/br/sghvet/controller/UiLoginController.java
@@ -1,5 +1,20 @@
 package br.sghvet.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.AnchorPane;
+
 public class UiLoginController {
+	
+	@FXML
+	private AnchorPane login_component;
+	
+	
+	
+	
+	DropShadow dropshadow = new DropShadow();
+	 
+	 
+	 
 
 }
