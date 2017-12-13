9a2117437f2c28ecad0da0236d07a0a8dc80017d
Daniel Freire <danjfreire@gmail.com>
2017-12-12 22:05:00 -0200

    Controlador consulta

---
+++ src/br/sghvet/controller/CadastroConsulta.java
@@ -0,0 +1,54 @@
+package br.sghvet.controller;
+
+import java.sql.Connection;
+import java.util.List;
+
+import br.sghvet.data.IRepositorioConsulta;
+import br.sghvet.data.RepositorioConsulta;
+import br.sghvet.model.Consulta;
+
+public class CadastroConsulta implements ICadastroConsulta{
+
+	private IRepositorioConsulta repo;
+	
+	public CadastroConsulta() {
+		this.repo = new RepositorioConsulta();
+	}
+	
+	@Override
+	public void conectar(Connection conect) {
+		this.repo.conectar(conect);
+	}
+	
+	@Override
+	public boolean cadastrarConsulta(Consulta consulta) throws Exception {
+		return repo.cadastrarConsulta(consulta);
+	}
+
+	@Override
+	public boolean removerConsulta(Consulta consulta) throws Exception {
+		return repo.removerConsulta(consulta);
+	}
+
+	@Override
+	public boolean atualizarConsulta(Consulta consulta) throws Exception {
+		return repo.atualizarConsulta(consulta);
+	}
+
+	@Override
+	public List<Consulta> buscarConsultaCpf(String cpf) throws Exception {
+		return repo.buscarConsultaCpf(cpf);
+	}
+
+	@Override
+	public List<Consulta> buscarConsultaVet(String cpf) throws Exception {
+		return repo.buscarConsultaVet(cpf);
+	}
+
+	@Override
+	public List<Consulta> buscarConsultaPro(int prontuario) throws Exception {
+		return repo.buscarConsultaPro(prontuario);
+	}
+
+
+}
