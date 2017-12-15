1faa3ddedf9aea45913c6380c58277a31b89d480
raylison100 <Ray99291072*>
2017-12-14 22:24:19 -0300

    ADD2 MANUAL
    ADD 2

--- src/br/sghvet/controller/Conexao.java
+++ src/br/sghvet/controller/Conexao.java
@@ -1,22 +1,49 @@
+//package br.sghvet.controller;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//import exceptions.ConectionException;
+//
+//public class Conexao {
+//
+//	private String projeto = "jdbc:mysql://localhost:3306/sghvet";
+//
+//	public Connection getConexao(String usuario, String senha) throws ConectionException {
+//
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver").newInstance();
+//			return DriverManager.getConnection(projeto, usuario, senha);
+//
+//		} catch (Exception e) {
+//			throw new ConectionException();
+//		}
+//}
+//}
+
+
+
 package br.sghvet.controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import exceptions.ConectionException;
-
 public class Conexao {
 
-	private String projeto = "jdbc:mysql://localhost:3306/sghvet";
+	private String host = "localhost";
+	private String projeto = "jdbc:mysql://"+host+":3306/sghvet";
 
-	public Connection getConexao(String usuario, String senha) throws ConectionException {
+	public Connection getConexao(String usuario, String senha) throws Exception {
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(projeto, usuario, senha);
 
-		} catch (Exception e) {
-			throw new ConectionException();
+		} catch (SQLException | ClassNotFoundException e) {
+			throw e;
 		}
 }
+	public String getHost() {
+		return host;
+	}
 }
