51f892912b0bfd229e067cea63c1acf85aec6dd5
merciof <mercio.andrade@ufrpe.br>
2018-01-12 23:11:50 -0300

    controlador e interface registro consulta

---
+++ src/br/sghvet/controller/IcontroleRegistroConsulta.java
@@ -0,0 +1,19 @@
+package br.sghvet.controller;
+
+import java.sql.Connection;
+
+import br.sghvet.model.RegistroConsulta;
+import br.sghvet.model.Consulta;
+
+/**
+ * IcontroleRegistroFunctionario
+ */
+public interface IcontroleRegistroConsulta {
+     
+    public RegistroConsulta buscarRegistro(Consulta consulta) throws Exception;
+	public boolean cadastrarRegistro(RegistroConsulta registroConsulta) throws Exception;
+	public boolean atualizarRegistro(RegistroConsulta registroConsulta) throws Exception;     
+
+    public void conectar(Connection conect);
+
+}
